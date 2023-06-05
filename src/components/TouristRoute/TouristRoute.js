68117253cbacf53c14755a57de3e595367dc20af
RadGreen <135602151+RadGreen@users.noreply.github.com>
2023-06-05 18:47:17 +0300

    reverted changes from practical 5

--- src/components/TouristRoute/TouristRoute.js
+++ src/components/TouristRoute/TouristRoute.js
@@ -1,46 +1,17 @@
-import {useState} from 'react'
+import React from 'react';
 import './TouristRoute.style.scss'
 
 function TouristRoute(props) {
-    const [isSelectRouteClicked, setIsSelectRouteClicked] = useState(false);
-    const [isDeletRouteClicked, setIsDeletRouteClicked] = useState(false);
+  const { name, cost, startDate, endDate } = props;
 
-    const { name, cost, startDate, endDate } = props;
-
-    function onSelectCheckboxClick() {
-        setIsSelectRouteClicked(!isSelectRouteClicked)
-    }
-
-    function onDeleteCheckboxClick(){
-        setIsDeletRouteClicked(!isDeletRouteClicked) 
-    }
-
-    return (
-        <li className='TouristRoute'>
-        <h2>{name}</h2>
-        <p>Cost: {cost}</p>
-        <p>Start date: {startDate}</p>
-        <p>End date: {endDate}</p>
-        <div className='TouristRoute-Actions'>
-            <label>
-                Select the route
-                <input 
-                    type='checkbox'
-                    checked={isSelectRouteClicked}
-                    onClick={onSelectCheckboxClick} 
-                />
-            </label>
-            <label>
-                Delete the route
-                <input 
-                    type='checkbox' 
-                    checked={isDeletRouteClicked}
-                    onClick={onDeleteCheckboxClick} 
-                />
-            </label>
-        </div>
-        </li>
-    );
+  return (
+    <li className='TouristRoute'>
+      <h2>{name}</h2>
+      <p>Cost: {cost}</p>
+      <p>Start date: {startDate}</p>
+      <p>End date: {endDate}</p> 
+    </li>
+  );
 };
 
 export default TouristRoute;
