d2183a2f24dcc0705f0c08f61e5bacdc182cf5be
RadGreen <135602151+RadGreen@users.noreply.github.com>
2023-06-05 18:42:34 +0300

    added checkboxes and state

--- src/components/MainContent/MainContent.js
+++ src/components/MainContent/MainContent.js
@@ -6,22 +6,22 @@ function MainContent() {
         <ul className='MainContent'>
             <h1>Tourist Routes List</h1>
             <TouristRoute
-            name="Trip to Italy"
-            cost='350 EUR' 
-            startDate='05 June'
-            endDate='15 June'
+                name="Trip to Italy"
+                cost='350 EUR' 
+                startDate='05 June'
+                endDate='15 June'
             />
             <TouristRoute
-            name="Trip to Great Britain"
-            cost='400 EUR' 
-            startDate='14 June'
-            endDate='23 June'
+                name="Trip to Great Britain"
+                cost='400 EUR' 
+                startDate='14 June'
+                endDate='23 June'
             />
             <TouristRoute
-            name="Trip to Poland"
-            cost='230 EUR' 
-            startDate='08 June'
-            endDate='17 June'
+                name="Trip to Poland"
+                cost='230 EUR' 
+                startDate='08 June'
+                endDate='17 June'
             />
         </ul>
     )
