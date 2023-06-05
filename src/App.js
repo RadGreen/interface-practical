0c29949a0b922ad73cd162c49494435e075d8e1a
RadGreen <135602151+RadGreen@users.noreply.github.com>
2023-06-05 14:37:35 +0300

    Added my info component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,11 @@
+import MyInfo from './components/MyInfo';
 import './App.scss'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <ul>
-          <li>Trip to Italy</li>
-          <li>Trip to Poland</li>
-          <li>Trip to Chechz Republic</li>
-          <li>Trip to Great Britain</li>
-        </ul>
+        <MyInfo/>
       </header>
     </div>
   );
---
+++ src/components/MyInfo/MyInfo.js
@@ -0,0 +1,15 @@
+function MyInfo() {
+    return (
+        <section>
+            <p>Personal information: Radyslav Grinchak 0936573341</p>
+            <ul>
+                <li>Trip to Italy</li>
+                <li>Trip to Poland</li>
+                <li>Trip to Chechz Republic</li>
+                <li>Trip to Great Britain</li>
+            </ul>
+        </section>
+    );
+}
+
+export default MyInfo;
