a16b647c4419989fe53c8ce37a765944e1cb67fe
RadGreen <135602151+RadGreen@users.noreply.github.com>
2023-06-05 18:42:34 +0300

    added checkboxes and state

--- src/components/TouristRoute/TouristRoute.js
+++ src/components/TouristRoute/TouristRoute.js
@@ -1,17 +1,46 @@
-import React from 'react';
+import {useState} from 'react'
 import './TouristRoute.style.scss'
 
 function TouristRoute(props) {
-  const { name, cost, startDate, endDate } = props;
+    const [isSelectRouteClicked, setIsSelectRouteClicked] = useState(false);
+    const [isDeletRouteClicked, setIsDeletRouteClicked] = useState(false);
 
-  return (
-    <li className='TouristRoute'>
-      <h2>{name}</h2>
-      <p>Cost: {cost}</p>
-      <p>Start date: {startDate}</p>
-      <p>End date: {endDate}</p> 
-    </li>
-  );
+    const { name, cost, startDate, endDate } = props;
+
+    function onSelectCheckboxClick() {
+        setIsSelectRouteClicked(!isSelectRouteClicked)
+    }
+
+    function onDeleteCheckboxClick(){
+        setIsDeletRouteClicked(!isDeletRouteClicked) 
+    }
+
+    return (
+        <li className='TouristRoute'>
+        <h2>{name}</h2>
+        <p>Cost: {cost}</p>
+        <p>Start date: {startDate}</p>
+        <p>End date: {endDate}</p>
+        <div className='TouristRoute-Actions'>
+            <label>
+                Select the route
+                <input 
+                    type='checkbox'
+                    checked={isSelectRouteClicked}
+                    onClick={onSelectCheckboxClick} 
+                />
+            </label>
+            <label>
+                Delete the route
+                <input 
+                    type='checkbox' 
+                    checked={isDeletRouteClicked}
+                    onClick={onDeleteCheckboxClick} 
+                />
+            </label>
+        </div>
+        </li>
+    );
 };
 
 export default TouristRoute;
