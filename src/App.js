6fdd6ff0fa61bc191efb014bf73a4079e586cbd4
RadGreen <135602151+RadGreen@users.noreply.github.com>
2023-06-05 14:14:18 +0300

    created cra and added list

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import './App.scss'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <ul>
+          <li>Trip to Italy</li>
+          <li>Trip to Germany</li>
+          <li>Trip to Poland</li>
+          <li>Trip to Chechz Republic</li>
+          <li>Trip to Great Britain</li>
+        </ul>
+      </header>
+    </div>
+  );
+}
+
+export default App;
